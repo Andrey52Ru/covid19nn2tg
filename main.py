83a2b0eea4509f85402b914e74b37823d3ee6513
Andrey52Ru <barinov.ru@gmail.com>
2020-04-19 16:30:05 +0300

    logging

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ from secrets import TG_TOKEN
 from vk import get_posts
 import logging
 
+
+logger = logging.getLogger(r"covid19nn2tg_bot")
+formatter = logging.Formatter(
+    '%(asctime)s (%(filename)s:%(lineno)d %(threadName)s) %(levelname)s - %(name)s: "%(message)s"'
+)
+
 bot = telebot.TeleBot(TG_TOKEN, threaded=False)
 telebot.logger.setLevel(logging.DEBUG)
 
 chat_id = set()
 sent_posts = set()
 
-fn_sent_posts = "./sent_posts.txt"
-fn_chat_ids = "./chats.txt"
+fn_sent_posts = r"./sent_posts.txt"
+fn_chat_ids = r"./chats.txt"
 
 
 # @bot.message_handler(content_types=['text'])
@@ -20,10 +26,10 @@ fn_chat_ids = "./chats.txt"
 #     pass
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def start_message(message):
+    logging.debug(f'New chat {message.chat.id}...')
     if message.chat.id not in chat_id:
-        print(type(message.chat.id))
         chat_id.add(message.chat.id)
         # save to file
         f = open(fn_chat_ids, 'w')
@@ -31,18 +37,17 @@ def start_message(message):
             for item in chat_id:
                 f.write('%s\n' % item)
         except NameError as e:
-            print(e)
+            logging.error(r"Exception: " + e)
             bot.send_message(message.chat.id, "Try again later")
         else:
-            print(f'New chat ({message.chat.id})')
+            logging.info(f'Success: new chat {message.chat.id}. Total: {len(chat_id)}')
         finally:
             f.close()
         bot.send_message(message.chat.id, "Started")
 
-
 @bot.message_handler(commands=['stop'])
 def stop_message(message):
-    print(f'Removing chat {message.chat.id}')
+    logging.debug(f'Removing chat {message.chat.id}...')
     chat_id.remove(message.chat.id)
     # save to file
     f = open(fn_chat_ids, 'w')
@@ -50,24 +55,25 @@ def stop_message(message):
         for item in chat_id:
             f.write('%s\n' % item)
     except NameError as e:
-        print(e)
+        logging.error(r"Exception: " + e)
         bot.send_message(message.chat.id, f"{e}\nTry again later")
     else:
-        print(f'Chat is removed ({message.chat.id})')
+        logging.info(f'Success: removed chat {message.chat.id}. Total: {len(chat_id)}')
     finally:
         f.close()
     bot.send_message(message.chat.id, "Stop")
 
 
-def send_msg(msg, media):
+def send_msg(post_id, msg, media):
+    logging.info(f'Sending message {post_id}...')
     for chat in chat_id:
-        print(f'\nMessage to {chat}:')
-        print(f'\n\t{msg}')
+        logging.debug(f'\nMessage to chat {chat}:')
         if len(media) > 0:
             bot.send_message(chat_id=chat, text=msg + '\n[ссылка](' + ')\n[ссылка]('.join(media) + ')',
                              disable_web_page_preview=False, parse_mode='Markdown')
         else:
             bot.send_message(chat, msg)
+    logging.debug('Post: {}\n\t{}\n\tMedia:'.format(post_id, msg, '\n'.join(media)))
 
 
 # echo
@@ -81,15 +87,13 @@ def get_new_posts(args):
         posts = get_posts(args["url"])
         for post_id in list(posts.keys())[::-1]:    # reverse
             if post_id not in sent_posts:
-                print(f'Sending {post_id}')
-                # print('_' * 30)
-                # print(f"{post_id}:\n\t{posts[post_id]['date']}\n\t{posts[post_id]['text']}\n\t{posts[post_id]['media_url']}")
-                # # print(posts[post_id]['media'])
-                # print('=' * 30)
-                send_msg(posts[post_id]['text'], posts[post_id]['media_url'])
+                send_msg(post_id, posts[post_id]['text'], posts[post_id]['media_url'])
                 sent_posts.add(post_id)
         save_sent_posts()
-        sleep(args['posts_interval'])
+        for i in range(args['posts_interval']):
+            if not args["run"]:
+                break
+            sleep(1)
 
 
 def run_bot(args):
@@ -103,7 +107,7 @@ def save_sent_posts():
             for item in sent_posts:
                 f.write('%s\n' % item)
     except NameError as e:
-        print(e)
+        logging.error(r"Exception: " + e)
 
 
 def load_data():
@@ -114,9 +118,9 @@ def load_data():
             # delete \n and append
             sent_posts.add(line[:-1])
     except FileNotFoundError:
-        print(f"\t File not found: {fn_sent_posts}")
+        logging.warning(f"\t File not found: {fn_sent_posts}")
     except NameError as e:
-        print(e)
+        logging.error(r"Exception: " + e)
     finally:
         if f:
             f.close()
@@ -126,9 +130,9 @@ def load_data():
         for line in f:
             chat_id.add(int(line))
     except FileNotFoundError:
-        print(f"\t File not found: {fn_chat_ids}")
+        logging.warning(f"\t File not found: {fn_chat_ids}")
     except NameError as e:
-        print(e)
+        logging.error(r"Exception: " + e)
     finally:
         if f:
             f.close()
@@ -140,7 +144,7 @@ if __name__ == '__main__':
             'posts_interval': 180}
 
     load_data()
-    print('Chats: \n\t{}'.format("\n\t".join(str(x) for x in chat_id)))
+    logging.info('Chats: \n\t{}'.format("\n\t".join(str(x) for x in chat_id)))
 
     # run_bot_thread = Thread(target=bot.infinity_polling(), args=(True,), daemon=True)
     get_posts_thread = Thread(target=get_new_posts, args=(info,), daemon=True)
