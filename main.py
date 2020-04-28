e0cf9f0dc9b17143b37dde639cdc450b4dde7b9d
Andrey52Ru <barinov.ru@gmail.com>
2020-04-28 21:44:02 +0300

    debug

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def send_post(bot, post_id, msg, media):
         else:
             ret_msg = bot.send_message(chat, msg)
         logger.debug(f'Posted to chat #{i} of {len(chats)}: {chat} Success: {ret_msg is not None}')
-    logger.debug('Post: {}\n\t{}\n\tMedia:'.format(post_id, msg, '\n'.join(media))[:50])
+    logger.debug('Post: {}\n\t{}\n\tMedia:'.format(post_id, msg, '\n'.join(media)))
 
 
 # echo
